4137acf44c346f070945529c3c1301a06faf25b1
anmefe2122daw2 <anmefe2122daw2@protonmail.com>
2022-05-12 16:52:41 +0200

    Segon commit del projecte zendldap

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-    require 'vendor/autoload.php';
-    use Laminas\Ldap\Ldap;
-    
+require 'vendor/autoload.php';
+use Laminas\Ldap\Ldap;
+ini_set('display_errors',0);
+if ($_GET['usr'] && $_GET['ou']){
     $domini = 'dc=fjeclot,dc=net';
     $opcions = [
         'host' => 'zend-anmefe',
@@ -10,12 +11,29 @@
         'bindRequiresDn' => true,
         'accountDomainName' => 'fjeclot.net',
         'baseDn' => 'dc=fjeclot,dc=net',
-     ];
+    ];
     $ldap = new Ldap($opcions);
     $ldap->bind();
-    $usuari=$ldap->getEntry('uid=sysdev,ou=desenvolupadors,dc=fjeclot,dc=net');
+    $entrada='uid='.$_GET['usr'].',ou='.$_GET['ou'].',dc=fjeclot,dc=net';
+    $usuari=$ldap->getEntry($entrada);
     echo "<b><u>".$usuari["dn"]."</b></u><br>";
     foreach ($usuari as $atribut => $dada) {
-     if ($atribut != "dn") echo $atribut.": ".$dada[0].'<br>';
+        if ($atribut != "dn") echo $atribut.": ".$dada[0].'<br>';
     }
+}
 ?>
+<html>
+    <head>
+        <title>
+        	MOSTRANT DADES D'USUARIS DE LA BASE DE DADES LDAP
+        </title>
+    </head>
+    <body>
+        <form action="http://zend-anmefe/zendldap" method="GET">
+        Unitat organitzativa: <input type="text" name="ou"><br>
+        Usuari: <input type="text" name="usr"><br>
+        <input type="submit"/>
+        <input type="reset"/>
+        </form>
+    </body>
+</html>
